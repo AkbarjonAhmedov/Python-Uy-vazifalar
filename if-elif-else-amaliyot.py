e6ea99972d4f7e73148c69b7b323a65044afb972
AkbarjonAhmedov <97118478+AkbarjonAhmedov@users.noreply.github.com>
2022-01-10 22:46:08 +0500

    Yangi fayl qo'shiladi
    if-elif else

---
+++ if-elif-else-amaliyot.py
@@ -0,0 +1,91 @@
+# 1-misol
+# son=int(input("Juft son kiriting: "))
+# if son%2==0: print("Rahmat")
+# else: print("Bu son juft emas")  
+
+# 2-misol
+# yosh = int(input("Yoshingizni kiriting: "))
+# if yosh<4 or yosh>60:
+#     print("Sizga kirish bepul")
+# elif yosh<18:
+#     print("Sizga kirish 10000 so'm")
+# else:
+#     print("Sizga kirish 20000 so'm")
+
+# 3-misol
+# son1=int(input("1-sonni kiriting: "))
+# son2=int(input("2-sonni kiriting: "))
+# if son1==son2:
+#     print("Bu sonlar teng")
+# elif son1>son2:
+#     print(f"{son1} > {son2}") 
+# else: print(f"{son1} < {son2}")
+
+ #4-misol
+# mahsulotlar=['sabzi','kartoshka','piyoz','un','guruch','yog','zira']
+# savat=[]
+
+# print('5 ta mahsulot kiriting: ')
+# for i in range(5):
+#     savat.append(input(f'{i+1}-mahsulot: '))
+
+# for meva in savat:
+#     if meva in mahsulotlar:
+#         print(f"do'konimizda {meva} bor")
+#     else:
+#         print(f"do'konimizda {meva} yo'q")
+
+        
+        
+  #5-misol
+# mahsulotlar=['sabzi','kartoshka','piyoz','un','guruch','yog','zira']
+# savat=[]
+# bor_mahsulotlar=[]
+# mavjud_emas=[]
+
+# print('3 ta mahsulot kiriting: ')
+# for i in range(3):
+#     savat.append(input(f'{i+1}-mahsulot: '))
+# for meva in savat:
+#         if meva.lower() in mahsulotlar:
+#             bor_mahsulotlar.append(meva.lower())
+#         else:
+#             mavjud_emas.append(meva.lower())
+# if mavjud_emas:
+#     print("Quyidagi mahsulotlar do'konimizda yo'q: ")
+#     for k in mavjud_emas:
+#         print(k)
+# else:
+#     print("Siz so'ragan barcha mahsulotlar do'konimizda bor")        
+        
+
+# # 6-misol
+# foydalanuvchilar=['men','sen','biz','siz','ular']
+# login=input("Yangi login kiriting: ")
+# if login.lower() in foydalanuvchilar:
+#     print("Login band, yangi login tanlang")
+# else:
+#     print(f"Hush kelibsiz, {login.capitalize()}")
+        
+ # 7-misol
+
+# son=int(input("Biror burun son kiriting: "))
+# for i in range(2,11):
+#     if not son%i:
+#         print(f"{son} soni {i} ga qoldiqsiz bo'linadi ")
+        
+# 1
+# son=float(input("Juft son kiriting: "))
+# if son%2:
+#     print('Bu juft emas')
+# else:
+#     print('Rahmat!')
+        
+        
+        
+        
+        
+        
+        
+        
+        
